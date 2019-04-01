89e4f5391992f362174b65899c37ae5f09d7c2ff
jynnie <jynnie@mit.edu>
2019-04-01 19:04:55 -0400

    switching the port to 80

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-const PORT = 3000;
+const PORT = 80;
 
 app.use("/static", express.static(path.join(__dirname, "public")));
 
